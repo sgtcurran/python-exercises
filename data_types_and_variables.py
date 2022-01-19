4291c6b919f8ad2d15564b2e9c5a2376cc0e8a47
christopher <christopher.curran24@gmail.com>
2022-01-19 12:21:20 -0600

    version 2

---
+++ data_types_and_variables.py
@@ -0,0 +1,76 @@
+#%%
+#x1 = 'the little mermaid' 
+#x2 = 'brother bear'
+#x3 = 'hercules'
+
+from pickle import FALSE
+
+x1 = (3 * 3) 
+x2 = (3 * 5) 
+x3 = (3 * 1) 
+
+x1 + x2 + x3
+#%%
+#x1 amazon = 380
+#x2 google = 400
+#x3 facebook = 350
+
+x1 = (380)
+x2 = (400)
+x3 = (350)
+
+(x1*4) + (x2*6) + (x3*10)
+
+#%%
+s = 'student'
+c = 'class'
+cc = 'current classes'
+cs = 'class schedual'
+cf = 'class full'
+cr = 'can enroll'
+
+if c > cf:
+    FALSE
+if cc >= cs:
+    FALSE
+if True:
+     print("Can Enroll")
+else:
+     print ("Can Not Enroll")
+
+#%%
+
+# product
+
+#%%
+
+#username = 'codeup'
+#password = 'notastrongpassword'
+
+#un = username
+#pw = password
+
+
+pw = 'notastrongpassword'
+if(len(pw)>=5):
+    True 
+else: FALSE
+    
+un = 'codeup'
+if(len(un)<=20):
+    True
+else: FALSE
+
+un_and_pw_diff = un != pw 
+un_has_space = un != un.strip()
+pw_has_space = pw != pw.strip()
+
+if True:
+    print("good")
+else:
+    print("not good")
+
+        
+
+#%%
+
