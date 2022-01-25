33ed97be7571fb9c1523fd84a57d548faeeb8fc4
christopher <christopher.curran24@gmail.com>
2022-01-24 20:27:06 -0600

    complete

--- function_exercises.py
+++ function_exercises.py
@@ -1,12 +1,15 @@
 #%%
 #1
 from curses.ascii import isdigit
+from doctest import OutputChecker
 from email import charset
+
+from numpy import cumproduct, index_exp
 
 
 def is_two(x):
-    if x == 2 or x =='two':
-        return True
+    if x == 2 or x =='two': # if conditon x is == 2 or str 'two' 
+        return True #returns true
     else:
         return False
 x = 2
@@ -18,8 +21,8 @@ print(is_two(x1))
 print(is_two(x2))
 # %%
 #2
-def is_vowel(char):
-    result = char.lower() in 'aeiou'
+def is_vowel(char): # char = characters 
+    result = char.lower() in 'aeiou' #results = character.lower() in 'aeiou'
     return result
      
 
@@ -31,27 +34,24 @@ print(is_vowel(x1))
 print(is_vowel(x2))
 # %%
 #3
-def is_consonant(char):
-    return char.lower() in 'zbtgh'
-x = 'zbtgh'
+def is_consonant(char): #variable is any character
+    return char.lower() in 'zbtgh' # if character is lower return true if 
+x = 'zbtgh' #x is a string 
 x1 = 'hi'
 print(is_consonant(x))
 print(is_consonant(x1))
 
 # %%
 #4
-
+# 
 def upper_con(string):
-    if type(string) != str:
+    if type(string) != str: # if type of string is not equal to string return false
         return False
-    first_letter = string[0]
-    if is_consonant(first_letter):
-        string = string.capitalize()
-        return string 
-     
-#if x =='z'or'b'or't'or'g'or'h'or'Z'or'B'or'T'or'G'or'H':    
-#char.upper()
-        
+    first_letter = string[0] # variable is first letter & placement 0
+    if is_consonant(first_letter): # calls is_consonant function applies variable first_letter
+        string = string.capitalize() # string = string.capitalize() makes first letter capitalize
+        return string #return if a consonant and makes the first letter capitalize
+            
 str1 = 'zebra'
 str2 = 'apple'
 str3 = 'boat'
@@ -60,12 +60,14 @@ print(upper_con(str2))
 print(upper_con(str3))
 # %%
 #5 user based input of bill plus 20% tip
+#Tip is the fixed variable of 0.20 & user_input must be a float or an error
+#mesage will occur. 
 user_input=float(input('enter total bill to get tip'))
 def calculate_tip(tip):
     tip = 0.20
     
-    return float(tip * user_input) + (user_input)
-
+    return float(tip*user_input)+user_input
+#return function is 0.20 * input + user_input to get correct output
 print('bill plus 20% tip =', calculate_tip(user_input))
 #%%
 #6
@@ -78,3 +80,68 @@ price = 10
 print('price = $10 Discount - 20% = ', apply_discount(discount))
 
 # %%
+#7 accept a string that is a number that contains 
+# commas in it as input, and return a number as output
+def handle_commas(string):
+    s = string.replace(',','')
+    return s
+
+string = ('one, two, three, four, five')
+
+print('remove comas from string:', handle_commas(string))
+# %%
+#8 accept a string that is a number that contains 
+# commas in it as input, and return a number as output
+
+def get_letter_grade(grade):
+    if grade >= 90:
+        return "A" 
+    elif grade >= 80:
+        return "B"
+    elif grade >= 70:
+        return "C"
+    elif grade >= 60:
+        return "D"
+    else:
+        return "F"
+        
+grade1 = 65 # variable
+
+print('Grade:', get_letter_grade(grade1)) # print function of get_letter_grand and independant varible 
+    
+# %%
+#9 accepts a string and returns a string with all 
+# the vowels removed.
+import re
+#import re (regular expression) & use re.sub to remove str elements
+def remove_vowels(vowels):
+    return (re.sub("[aeiouAEIOU]","", vowels))
+vowels1 = 'I came, I saw, I got blown up'
+
+print('I came, I saw, I got blown up','=', remove_vowels(vowels1))
+# %%
+#10 
+# user inputs string 
+string = input('Enter your first name').lower() #converts string input to lower case
+def normalize_name(string):
+    output = '' #output 
+    for character in string: # for characters in string 
+        if character.isidentifier() or character == ' ':#isidentifier() returns true if valid identifier or character == ' '
+            output += character
+    output = output.strip() #.strip removes whitespace
+    output = output.replace(' ', '_') #.replace whitespace between strings & adds _
+
+    return output
+print(normalize_name(string))
+    
+# %%
+#11
+import numpy as np 
+# import numpy as np to use tool cumsum
+def cumulative_sum(list):# variable is any list
+    return np.cumsum(list) # return numpy cumsum of any list
+
+print(cumulative_sum([1, 1, 1]))
+print(cumulative_sum([1, 2, 3, 4]))
+# %%
+
